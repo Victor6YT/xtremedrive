4bf9c064f21347e1c52355831c7e29905cd5a5c3
Victor6YT <163203584+Victor6YT@users.noreply.github.com>
2024-03-12 22:01:03 +0100

    Create QuitButton.gd
    A quit button that closes game in godot

---
+++ QuitBurron.gd
@@ -0,0 +1,10 @@
+extends Button
+
+# This is the quit button
+func _ready():
+# warning-ignore:return_value_discarded
+		connect("pressed", self, "_on_button_pressed")
+	
+func _on_button_pressed():
+	get_tree().quit()
+	
